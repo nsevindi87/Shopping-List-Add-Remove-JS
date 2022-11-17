0b3672e8f8062e323a0b5b48e80c78b83bf3e759
Nizami Sevindi <nsevindi87@gmail.com>
2022-11-17 16:38:30 +0100

    Shopping List Documents

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const form = document.getElementById("form")
-const item = document.querySelector("#item")
-const list = document.querySelector(".list")
-const listBody = document.querySelector(".list-body")
-const deleteAll = document.querySelector(".delete-all")
+const form = document.getElementById("form");
+const item = document.querySelector("#item");
+const list = document.querySelector(".list");
+const listBody = document.querySelector(".list-body");
+const deleteAll = document.querySelector(".delete-all");
 
 eventListener();
 
@@ -11,6 +11,40 @@ function eventListener(){
         e.preventDefault();
         addItem();
     })
-    listBody.addEventListener("click", deleteItem)
+    listBody.addEventListener("click", deleteItem);
     deleteAll.addEventListener("click", deleteAllItem)
 }
+
+function addItem(){
+    newItem = item.value.trim();
+    if(newItem !== ""){
+        addItemToList(newItem)
+    }
+}
+
+function addItemToList(newItem){
+    const listItem = document.createElement("li")
+    const link = document.createComment("a")
+    const text = document.createComment("span")
+    link.href = "#";
+    link.className = "delete-item"
+    link.innerHTML ='<i class="fas fa-trash"></i>'
+    const newItemCreate = document.createTextNode(newItem);
+    text.appendChild(newItemCreate)
+    link.appendChild(text)
+    listItem.appendChild(link)
+    list.appendChild(listItem)
+    item.value=""
+}
+
+function deleteItem(e){
+    if(e.target.className === "fas fa-trash"){
+        e.target.parentElement.parentElement.remove()
+    }
+}
+
+function deleteAllItem(){
+    listBody.innerHTML = ""
+}
+
+/* APPENDCHILD HATA VERIYOR */
